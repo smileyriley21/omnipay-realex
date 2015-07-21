b0a6e2724bfbda2adf1d2ba069346b5dad148aca
Chris Riley <chris@facemediagroup.co.uk>
2015-07-21 16:38:55 +0100

    Cards now going into Realex. Hurrah

--- src/Omnipay/Realex/Message/CreateCardRequest.php
+++ src/Omnipay/Realex/Message/CreateCardRequest.php
@@ -15,11 +15,12 @@ class CreateCardRequest extends RemoteAbstractRequest
     protected $endpoint = 'https://epage.payandshop.com/epage-remote-plugins.cgi';
 
 
-    public function getCardReference(){
-        return $this->getParameter('cardRef');
+
+    public function getCardRef(){
+        return str_replace(' ', '', $this->getParameter('cardRef'));
     }
 
-    public function setCardReference($value){
+    public function setCardRef($value){
         $this->setParameter('cardRef', $value);
     }
 
@@ -32,8 +33,7 @@ class CreateCardRequest extends RemoteAbstractRequest
     public function getData()
     {
 
-
-        // $this->validate('amount', 'currency', 'transactionId');
+        //$this->validate('amount', 'currency', 'transactionId');
 
         // Build the hash string
         $timestamp = strftime("%Y%m%d%H%M%S");
@@ -41,13 +41,16 @@ class CreateCardRequest extends RemoteAbstractRequest
         $orderId = time() + rand(0, 1000); // Random number
         $payerRef = $this->getPayerReference();
 
+        // Get the credit card
+        $credit_card = $this->getCard();
 
         $secret = $this->getSecret();
-        $tmp = "$timestamp.$merchantId.$orderId...$payerRef";
+        $tmp = "$timestamp.$merchantId.$orderId..." . $payerRef . "." . $credit_card->getName() . "." . $credit_card->getNumber();
+
+
         $sha1hash = sha1($tmp);
         $tmp2 = "$sha1hash.$secret";
         $sha1hash = sha1($tmp2);
-
 
         // Its DOM time
         $domTree = new \DOMDocument('1.0', 'UTF-8');
@@ -58,23 +61,41 @@ class CreateCardRequest extends RemoteAbstractRequest
         $root->setAttribute('timestamp', $timestamp);
         $root = $domTree->appendChild($root);
 
+
+
         // merchant ID
         $root->appendChild($domTree->createElement('merchantid', $merchantId));
 
         // Unique id for this request (normally use timestamp + rand(1,5000) or similar
         $root->appendChild($domTree->createElement('orderid', $orderId));
 
+        // Create the card
+        $card =  $domTree->createElement('card');
+
         // Payer ref. The person this card is being assigned to
-        $root->appendChild($domTree->createElement('payerref', $this->getPayerReference()));
+        $card->appendChild($domTree->createElement('payerref', $this->getPayerReference()));
+
 
         // Card Reference (Customers name for it, e.g. Current Account)
-        $root->appendChild($domTree->createElement('cardref', $this->getCardRef()));
+        $card->appendChild($domTree->createElement('ref', $this->getCardRef()));
 
         // Card Number
-        $root->appendChild($domTree->createElement('number', $this->getNumber()));
+        $card->appendChild($domTree->createElement('number', $credit_card->getNumber()));
 
-        // Card Number
-        $root->appendChild($domTree->createElement('number', $this->getExpMonth() . $this->getExpYear()));
+        // Expiry date
+        $card->appendChild($domTree->createElement('expdate', $credit_card->getExpiryDate('my')));
+
+        // Card holder name
+        $card->appendChild($domTree->createElement('chname', $credit_card->getName()));
+
+        // Type or Brand (e.g. VISA - Auto calculated)
+        $card->appendChild($domTree->createElement('type', $credit_card->getBrand()));
+
+
+        $root->appendChild($card);
+
+        // Issue number?
+        // To add
 
         // Hash
         $sha1El = $domTree->createElement('sha1hash', $sha1hash);
@@ -82,13 +103,9 @@ class CreateCardRequest extends RemoteAbstractRequest
 
         $xmlString = $domTree->saveXML($root);
 
-        dd($xmlString);
-
         return $xmlString;
 
-
     }
-
 
     /**
      * Lets do this.. return the response
